ab7c4187628106bde34be4b83c23b70ca1f70a0a
DavidGau <gaulindavidweb@gmail.com>
2019-04-18 22:04:16 -0400

    Added pictures, changes catalog css

--- Catalog.php
+++ Catalog.php
@@ -34,57 +34,60 @@ Date Nom Description
 	 	integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	 </head>
 	 <body>
-	 	<header class="nav-bar"> <!-- Page header -->
-	 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
-	 			<a class="navbar-brand" href="#">
-	 				<img src="images/logo.png" alt="Quintessentiel logo" class="img-logo-nav" />
-	 			</a>
-	 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
-	 			aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-	 			<span class="navbar-toggler-icon"></span>
-	 		</button>
-	 		<div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNavDropdown">
-	 			<ul class="navbar-nav ml-auto">
-	 				<li class="nav-item active">
-	 					<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
-	 				</li>
-	 				<li class="nav-item">
-	 					<a class="nav-link" href="#">Produits</a>
-	 				</li>
-	 				<li class="nav-item">
-	 					<a class="nav-link" href="#">Nous joindre</a>
-	 				</li>
-	 			</ul>
-	 		</div>
-	 	</nav>
-	 </header>
-	 <section> <!-- Page section -->
-	 	<div class="page-title-bar">
-	 		<h1>Produits</h1>
-	 	</div>
+	 	<div class="page">
+		 	<header class="nav-bar"> <!-- Page header -->
+		 		<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		 			<a class="navbar-brand" href="#">
+		 				<img src="images/logo.png" alt="Quintessentiel logo" class="img-logo-nav" />
+		 			</a>
+		 			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown"
+		 			aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+		 			<span class="navbar-toggler-icon"></span>
+		 		</button>
+		 		<div class="navbar-collapse collapse w-100 order-3 dual-collapse2" id="navbarNavDropdown">
+		 			<ul class="navbar-nav ml-auto">
+		 				<li class="nav-item active">
+		 					<a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+		 				</li>
+		 				<li class="nav-item">
+		 					<a class="nav-link" href="#">Produits</a>
+		 				</li>
+		 				<li class="nav-item">
+		 					<a class="nav-link" href="#">Nous joindre</a>
+		 				</li>
+		 			</ul>
+		 		</div>
+		 	</nav>
+		 </header>
+		 <section class="main-section"> <!-- Page section -->
+		 	<div class="page-title-bar">
+		 		<h1>Produits</h1>
+		 	</div>
 
-	 	<section class="search-section"> <!-- search section -->
-	 		<div>	<!-- search bar -->
-	 			<input type="text" name="search" placeholder="Rechercher ..." id="searchBar"/>
-	 		</div>
-	 		<p>Filtrer par: </p>
-	 		<select name="filter" id="filter">
-	 			<option disabled selected value>sélectionner un filtre</option>
-	 			<option value="name">Nom</option>
-	 			<option value="price">Prix</option>
-	 			<option value="quantity">Quantité</option>
-	 		</select>
-	 	</section>
-	 	<section class="product-section"> <!-- products section -->
-	 		<?php
+		 	<section class="search-section"> <!-- search section -->
+		 		<div>	<!-- search bar -->
+		 			<input type="text" name="search" placeholder="Rechercher ..." id="searchBar"/>
+		 		</div>
+		 		<p>Filtrer par: </p>
+		 		<select name="filter" id="filter">
+		 			<option disabled selected value>sélectionner un filtre</option>
+		 			<option value="name">Nom</option>
+		 			<option value="price">Prix</option>
+		 			<option value="quantity">Quantité</option>
+		 		</select>
+		 	</section>
+		 	<section class="product-section"> <!-- products section -->
+		 		<?php
 
-			$ctrl->loadAllProducts();
-			$_SESSION["ctrlProduct"] = serialize($ctrl);
-			?>
-	 	</section>
-	 	<footer class="classic-footer">
-	 		<p>Insérez du texte ici</p>
-	 	</footer>
+				$ctrl->loadAllProducts();
+				$_SESSION["ctrlProduct"] = serialize($ctrl);
+				?>
+		 	</section>
+		 	<footer class="classic-footer">
+		 		<p>Insérez du texte ici</p>
+		 	</footer>
+		 </section>
+		</div>
 
 	 	<script>
 
@@ -110,7 +113,7 @@ Date Nom Description
 
 	 			$.ajax({
 	 				type: "POST",
-	 				url: "phpScripts/methodCall/scriptCallProductGetByName.php",
+	 				url: "phpScripts/methodCall/scriptCatalog.php",
 	 				data: {name: searchText,
 	 					   filter: selectedFilter,
 	 					},
@@ -121,10 +124,10 @@ Date Nom Description
 	 		}
 
 	 		function changePage(pageNumber){
-	 			console.log("ici.....")
+	 			
 	 			$.ajax({
 	 				type: "POST",
-	 				url: "phpScripts/methodCall/scriptCallProductGetByName.php",
+	 				url: "phpScripts/methodCall/scriptCatalog.php",
 	 				data: {changePage: pageNumber
 	 					},
 	 				success: function(output) {
--- phpScripts/Product/CtrlProduct.php
+++ phpScripts/Product/CtrlProduct.php
@@ -27,103 +27,147 @@ class CtrlProduct
         $this->itemPerPage = 10;
     }
 
-    //loads all the products from the db
+    
+    /**
+     * Loads all the products and
+     * displays it in tables
+     * */
     public function loadAllProductsTable()
     {
-        $this->pageNumber = 0;
-        $productList = $this->mgrProduct->getAllProducts();
+        $this->setPageNumber(0);
+        $productList = $this->getMgrProduct()->getAllProducts();
         $this->displayProductRows();
     }
 
-    //loads all the products from the db
+    
+    /**
+     * Loads every products and 
+     * displys it as a product
+     * $filter: ORDER BY $filter
+     * */
     public function loadAllProducts($filter = null)
     {
         $this->pageNumber = 0;
-        $productList = $this->mgrProduct->getAllProducts($filter);
+        $productList = $this->getMgrProduct()->getAllProducts($filter);
         $this->displayProduct();
     }
 
-    //Loads all the sellable products
+    
+    /**
+     * Loads all the sellables products
+     * */
     public function loadAllSellables()
     {
         $this->pageNumber = 0;
-        $sellableProductList = $this->mgrProduct->getAllSellables();
+        $sellableProductList = $this->getMgrProduct()->getAllSellables();
         $this->displayProduct();
     }
 
-    //Loads all products by name
+    /**
+     * Loads the products relative to
+     * the given letters
+     *
+     * $name: letters given by the user
+     * $filter: ORDER BY $filter
+     * */
     public function loadProductsByName($name, $filter = null)
     {
         $this->pageNumber = 0;
-        $productList = $this->mgrProduct->getProductsByName($name, $filter);
+        $productList = $this->getMgrProduct()->getProductsByName($name, $filter);
         $this->displayProduct();
     }
 
-    //Displays the products on the page
+    /**
+     * Displays the list of product
+     * */
     private function displayProduct()
     {
-        $products = $this->mgrProduct->getProduct();
+        $products = $this->getMgrProduct()->getProduct();
 
-        $from = $this->itemPerPage * $this->pageNumber; //Number of item skipped
-        $to = (sizeof($products) - $from >= $this->itemPerPage ? $from + $this->itemPerPage : sizeof($products)); //Number of item to display
-        $maxNumberOfPage = round(sizeof($products)/$this->itemPerPage);
+        $from = $this->getItemPerPage() * $this->getPageNumber(); //Number of item skipped
+        $to = (sizeof($products) - $from >= $this->getItemPerPage() ? $from + $this->getItemPerPage() : sizeof($products)); //Number of item to display
+        $maxNumberOfPage = round(sizeof($products) / $this->getItemPerPage());
 
         $html = "";
 
-        if (!empty($products)) {
+        if(!empty($products)) {
 
             for ($i = $from; $i < $to; $i++) {
+                $description = str_split($products[$i]->getDescription(),50);
+                $dots = (sizeof($description) > 1 ? "..." : ""); //if description is more than 50 chars
+                $description = (!empty($description) ? $description[0] : $description);
 
                 $html .= "<div class='product'>";
                 $html .= "<img src='" . $products[$i]->getImagePath() . "' alt='un produit'/>";
                 $html .= "<h2>" . $products[$i]->getName() . "</h2>";
-                $html .= "<p>" . $products[$i]->getDescription() . "</p>";
+                $html .= "<p>" .  $description . $dots ."</p>";
                 $html .= "<p class='bottom-text'><span class='stock'>" . $products[$i]->getQuantity() . " en stock</span>";
                 $html .= "<span class='prix'>" . $products[$i]->getPrice() . "</span></p>";
                 $html .= "</div>";
             }
 
-            $html .= "<div class='link-page-box'>";
-
-            //Page buttons
-            if($this->pageNumber > 0){  //Previous button
-                $html .= "<a href='#' title='page précédente' onclick='changePage(".($this->pageNumber - 1).")'>Précédent</a>";
-            }
-            
-
-            for ($j=0; $j < $maxNumberOfPage; $j++) { 
-
-                if($this->pageNumber == $j){    //Currently on this page
-                    $html .= "<a href='#' title='autre page' style='color:black;' onclick='changePage(".$j.")'>".($j + 1)."</a>"; 
-                }
-                else{   //Other pages
-                    $html .= "<a href='#' title='autre page' onclick='changePage(".$j.")'>".($j + 1)."</a>";
-                }
-                
-            }
-
-            if($this->pageNumber < $maxNumberOfPage - 1){   //Next button
-                $html .= "<a href='#' title='page suivante' onclick='changePage(".($this->pageNumber + 1).")'>Suivant</a>";
-            }
-            
-
-            $html .= "</div>";
-
         } else {
             $html .= "<p>Aucun item ne correspond!</p>";
         }
 
+        $html .= $this->genertePageButton($maxNumberOfPage);
+
         echo $html;
     }
 
-    //Displays the next products
+    /**
+     * Generates the 1,2,3,previous,next, etc buttons
+     * The number of buttons loaded is dynamicaly determined by
+     * the number of element to show
+     *
+     * $maxNumberOfPage: total number of pages
+     * number of elements to load divided by elements per page
+     * */
+    private function genertePageButton($maxNumberOfPage)
+    {
+        $html = "<div class='link-page-box'>";
+
+        //Page buttons
+        if ($this->getPageNumber() > 0) {
+            //Previous button
+            $html .= "<a href='#' title='page précédente' onclick='changePage(" . ($this->getPageNumber() - 1) . ");return false'>Précédent</a>";
+        }
+
+        for ($j = 0; $j < $maxNumberOfPage; $j++) {
+
+            if ($this->getPageNumber() == $j) {
+                //Currently on this page
+                $html .= "<a href='#' title='autre page' style='color:black;' onclick='changePage(" . $j . ");return false'>" . ($j + 1) . "</a>";
+            } else {
+                //Other pages
+                $html .= "<a href='#' title='autre page' onclick='changePage(" . $j . ");return false'>" . ($j + 1) . "</a>";
+            }
+
+        }
+
+        if ($this->getPageNumber() < $maxNumberOfPage - 1) {
+            //Next button
+            $html .= "<a href='#' title='page suivante' onclick='changePage(" . ($this->getPageNumber() + 1) . ");return false'>Suivant</a>";
+        }
+
+        $html .= "</div>";
+
+        return $html;
+    }
+
+    /**
+     * Changes the page number and
+     * loads the next elements
+     * */
     public function changePage($pageNumber)
     {
-        $this->pageNumber = $pageNumber;
+        $this->setPageNumber($pageNumber);
         $this->displayProduct();
     }
 
-    //Displays the products in rows on the page
+    /**
+     * Displays the product list in rows
+     * */
     private function displayProductRows()
     {
 
@@ -152,6 +196,7 @@ class CtrlProduct
 
         echo $html;
     }
+
     /**
      * @return mixed
      */
@@ -186,5 +231,23 @@ class CtrlProduct
     public function setItemPerPage($itemPerPage)
     {
         $this->itemPerPage = $itemPerPage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPageNumber()
+    {
+        return $this->pageNumber;
+    }
+
+    /**
+     * @param mixed $mgrProduct
+     *
+     * @return self
+     */
+    public function setPageNumber($pageNumber)
+    {
+        $this->pageNumber = $pageNumber;
     }
 }
