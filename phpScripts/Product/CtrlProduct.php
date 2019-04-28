7c39feb5590e803981de76906bc4530d3a05e09c
DavidGau <gaulindavidweb@gmail.com>
2019-04-28 17:29:27 -0400

    Fixed catalog image bug (#24)

--- phpScripts/Product/CtrlProduct.php
+++ phpScripts/Product/CtrlProduct.php
@@ -105,7 +105,7 @@ class CtrlProduct
                 $description = (!empty($description) ? $description[0] : $description);
 
                 $html .= "<a class='product' href='Item.php?productId=".$products[$i]->getId()."' title='plus d info'>";
-                $html .= "<img src='.\images\imgProducts\\" . $products[$i]->getImagePath() . "' alt='".$products[$i]->getName()."'/>";
+                $html .= "<img src='" . $products[$i]->getImagePath() . "' alt='".$products[$i]->getName()."'/>";
                 $html .= "<h2>" . $products[$i]->getName() . "</h2>";
                 $html .= "<p>" .  $description . $dots ."</p>";
                 $html .= "<p class='bottom-text'><span class='stock'>" . $products[$i]->getQuantity() . " en stock</span>";
