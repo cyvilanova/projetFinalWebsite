c74e6cc779b7273a10da817fd867c3ef544568a8
PhilippeAuditAllaire <warpaintingqc@gmail.com>
2019-04-30 13:54:16 -0400

    fix PR 30/4/2019

--- phpScripts/Client/Client.php
+++ phpScripts/Client/Client.php
@@ -23,116 +23,116 @@ class Client{
 
 
   /**
-   * Client constructor with parameters.
-   * @param  mixed $address the street address of the client
-   * @param  mixed $city the name of the city of the client
-   * @param  mixed $name the name of the client
-   * @param  mixed $postalCode the postal code of the client
-   * @param  mixed $province the province of the client
-   * @param  mixed $id the identification number of the client
-   */
+  * Client constructor with parameters.
+  * @param  mixed $address the street address of the client
+  * @param  mixed $city the name of the city of the client
+  * @param  mixed $name the name of the client
+  * @param  mixed $postalCode the postal code of the client
+  * @param  mixed $province the province of the client
+  * @param  mixed $id the identification number of the client
+  */
   public function __construct($address,$city,$name,$postalCode,$province,$id){
-      $this->address = $address;
-      $this->city = $city;
-      $this->name = $name;
-      $this->postalCode = $postalCode;
-      $this->province = $province;
-      $this->id = $id;
+    $this->address = $address;
+    $this->city = $city;
+    $this->name = $name;
+    $this->postalCode = $postalCode;
+    $this->province = $province;
+    $this->id = $id;
   }
 
 
   /**
-   * Gets the street address of the client
-   * @return string $address the address of the client
-   */
+  * Gets the street address of the client
+  * @return string $address the address of the client
+  */
   public function getAddress(){
-      return $this->address;
+    return $this->address;
   }
 
   /**
-   * Sets the street address of the client
-   * @param  mixed $newAddress
-   */
+  * Sets the street address of the client
+  * @param  mixed $newAddress
+  */
   public function setAddress($newAddress){
-      $this->address = $newAddress;
+    $this->address = $newAddress;
   }
 
   /**
-   * Gets the city of the client
-   * @return string $city the city of the client
-   */
+  * Gets the city of the client
+  * @return string $city the city of the client
+  */
   public function getCity(){
-      return $this->city;
+    return $this->city;
   }
 
   /**
-   * Sets the city of the client
-   * @param  mixed $neCity
-   */
+  * Sets the city of the client
+  * @param  mixed $neCity
+  */
   public function setCity($newCity){
-      $this->city = $newCity;
+    $this->city = $newCity;
   }
 
   /**
-   * Gets the name of the client
-   * @return string $name the name of the client
-   */
+  * Gets the name of the client
+  * @return string $name the name of the client
+  */
   public function getName(){
-      return $this->name;
+    return $this->name;
   }
 
   /**
-   * Sets the name of the client
-   * @param  mixed $newName
-   */
+  * Sets the name of the client
+  * @param  mixed $newName
+  */
   public function setName($newName){
-      $this->name = $newName;
+    $this->name = $newName;
   }
 
   /**
-   * Gets the postal code of the client
-   * @return string $postalCode the postal code of the client
-   */
+  * Gets the postal code of the client
+  * @return string $postalCode the postal code of the client
+  */
   public function getPostalCode(){
-      return $this->postalCode;
+    return $this->postalCode;
   }
 
   /**
-   * Sets the postal code of the client
-   * @param  mixed $newPostalCode
-   */
+  * Sets the postal code of the client
+  * @param  mixed $newPostalCode
+  */
   public function setPostalCode($newPostalCode){
-      $this->postalCode = $newPostalCode;
+    $this->postalCode = $newPostalCode;
   }
 
   /**
-   * Gets the province of the client
-   * @return string $province the province of the client
-   */
+  * Gets the province of the client
+  * @return string $province the province of the client
+  */
   public function getProvince(){
-      return $this->province;
+    return $this->province;
   }
 
   /**
-   * Sets the province of the client
-   * @param  mixed $newProvince
-   */
+  * Sets the province of the client
+  * @param  mixed $newProvince
+  */
   public function setProvince($newProvince){
-      $this->province = $newProvince;
+    $this->province = $newProvince;
   }
 
   /**
-   * Gets the id of the client
-   * @return string $id the id of the client
-   */
+  * Gets the id of the client
+  * @return string $id the id of the client
+  */
   public function getId(){
     return $this->id;
   }
 
   /**
-   * Sets the id of the client
-   * @param  mixed $newId
-   */
+  * Sets the id of the client
+  * @param  mixed $newId
+  */
   public function setId($newId){
     $this->id = $newId;
   }
--- phpScripts/User/MgrUser.php
+++ phpScripts/User/MgrUser.php
@@ -13,53 +13,52 @@ Date Nom Description
 =========================================================
  ****************************************/
 
- include_once "User.php";
- include_once "../QueryEngine.php";
+  include_once "User.php";
+  include_once "../QueryEngine.php";
 
 
-class MgrUser
-{
+  class MgrUser{
 
-  private $qe;  //query manager
+    private $qe;  //query manager
 
-  /**
-	 * Recipe constructor with no parameters.
-	 */
-  public function __construct(){
-  }
+    /**
+    * Recipe constructor with no parameters.
+    */
+    public function __construct(){
+    }
 
 
-	/**
-	 * Verifies the credentials provided by the user to see if the user is
-   * already signed up in the database
-	 * and returns true if found.
-   * @param mixed $user user passed to check for credentials
-	 * @return boolean if there is a match for the credentials in the database
-	 *
-	 */
-  public function connection($user){
-    $qe = new QueryEngine();  //Initialization of the QueryEngine
-    $usrname = $user->getUsername();
-    $pwd = $user->getPassword();
+    /**
+    * Verifies the credentials provided by the user to see if the user is
+    * already signed up in the database
+    * and returns true if found.
+    * @param mixed $user user passed to check for credentials
+    * @return boolean if there is a match for the credentials in the database
+    *
+    */
+    public function connection($user){
+      $qe = new QueryEngine();  //Initialization of the QueryEngine
+      $usrname = $user->getUsername();
+      $pwd = $user->getPassword();
 
-    //Creation of the array of parameters
-    $parameters =
-    [
+      //Creation of the array of parameters
+      $parameters =
+      [
       ":username"=>$usrname,
       ":password"=>$pwd,
-    ];
+      ];
 
-    $stmt = "SELECT * FROM user WHERE username =:username AND password =:password";
-    $rs = $qe->executeQuery($stmt, $parameters);
+      $stmt = "SELECT * FROM user WHERE username =:username AND password =:password";
+      $rs = $qe->executeQuery($stmt, $parameters);
 
-    //Verify if the rowcount of the returned result set it greater than 0
-    if($rs->rowCount()>0){
-      return true;
-    }
-    else{
-      return false;
+      //Verify if the rowcount of the returned result set it greater than 0
+      if($rs->rowCount()>0){
+        return true;
+        }
+      else{
+        return false;
+        }
     }
   }
-}
 
  ?>
