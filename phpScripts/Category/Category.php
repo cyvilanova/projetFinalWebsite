89884c5f54955323c9fdfa86aef28fb48ae1b0c1
PhilippeAuditAllaire <warpaintingqc@gmail.com>
2019-05-01 19:46:24 -0400

    insert.sql fix, category, mgr et ctrl.php

---
+++ phpScripts/Category/Category.php
@@ -0,0 +1,99 @@
+<?php
+/****************************************
+ Fichier : Category.php
+ Auteur : Philippe Audit-Allaire
+ Fonctionnalité : W - Connexion de l'utilisateur
+ Date : 2019-04-15
+ Vérification :
+ Date Nom Approuvé
+ =========================================================
+ Historique de modifications :
+ Date Nom Description
+ =========================================================
+****************************************/
+  class Category{
+    private $id; //Identification umber of the category
+    private $name;  //Name of the category
+    private $active;  //If the category is active
+    private $description; //Description of the category
+
+    /**
+    * Category constructor with parameters.
+    * @param  mixed $id the identification number of the Category
+    * @param  mixed $name the name of the Category
+    * @param  mixed $active the state of activity of the category
+    * @param  mixed $description the description of the category
+    */
+    public function __construct($id, $name, $active, $description){
+      $this->setId($id);
+      $this->setName($name);
+      $this->setActive($active);
+      $this->setDescription($description);
+    }
+
+    /**
+    *Gets the id of the category
+    * @return int $id of the category
+    */
+    public function getId(){
+      return $this->id;
+    }
+
+    /**
+    *Sets the id of the category
+    * @param mixed $id
+    */
+    public function setId($id){
+      $this->id = $id;
+    }
+
+    /**
+    *Gets the name of the category
+    * @return int $name of the category
+    */
+    public function getName(){
+      return $this->name;
+    }
+
+    /**
+    *Sets the name of the category
+    * @param mixed $name
+    */
+    public function setName($name){
+      $this->name = $name;
+    }
+
+    /**
+    *Gets the activity of the category
+    * @return int $active of the category
+    */
+    public function getActive(){
+      return $this->active;
+    }
+
+    /**
+    *Sets the activity of the category
+    * @param mixed $active
+    */
+    public function setActive($active){
+      $this->active = $active;
+    }
+
+    /**
+    *Gets the description of the category
+    * @return string $description of the category
+    */
+    public function getDescription(){
+      return $this->description;
+    }
+
+    /**
+    *Sets the description of the category
+    * @param mixed $description
+    */
+    public function setDescription($description){
+      $this->description = $description;
+    }
+  }
+
+  ?>
--- sql/insert.sql
+++ sql/insert.sql
@@ -28,25 +28,25 @@ INSERT INTO product(name, image_path, is_sellable, description, price)
 VALUES("Gant de crain", "img4.jpg", 1, "Action reparatrice", 2.78);
 
 
-
 /* recipe */
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(1, "B barbe lavande", 0, "1- Melanger la farine 2- Ajouter les herbes");
 
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(2, "B barbe sapin", 0, "1- Melanger la farine 2- Ajouter les herbes");
 
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(3, "Parfum rose", 1, "1- Melanger la farine 2- Ajouter les herbes 3- Sentir");
 
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(4, "Beurre mercure", 0, "1- Melanger la farine 2- Ajouter les herbes");
 
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(5, "Savon blanc", 0, "1- Melanger la farine 2- Ajouter les herbes");
 
-INSERT INTO recipe(id_product, name, is_custom, steps) 
+INSERT INTO recipe(id_product, name, is_custom, steps)
 VALUES(6, "Creme main orange", 1, "1- Melanger la farine 2- Ajouter les herbes");
+
 
 /* ta_recipe_product */
 INSERT INTO ta_recipe_product(id_recipe, id_product, qty_ml)
@@ -66,3 +66,42 @@ VALUES(5,4,0.1);
 
 INSERT INTO ta_recipe_product(id_recipe, id_product, qty_ml)
 VALUES(5,5,22);
+
+
+/*User*/
+INSERT INTO user(id_question, username, password, email, secret_answer)
+VALUES(1,"test","test123","warpaintingqc@gmail.com","chopin");
+
+INSERT INTO user(id_question, username, password, email, secret_answer)
+VALUES(2,"test!@#$%","test!@#$%","zebulon@gmail.com","corin");
+
+INSERT INTO user(id_question, username, password, email, secret_answer)
+VALUES(3,"AlloToi","byebye","lolnon@gmail.com","Audit");
+
+
+/*secret_question*/
+INSERT INTO secret_question(question)
+VALUES("Quel était le nom de votre premier animal de compagnie?");
+
+INSERT INTO secret_question(question)
+VALUES("Quel est le nom de votre premier(e) meilleur(e) ami(e)?");
+
+INSERT INTO secret_question(question)
+VALUES("Quel était le nom de jeune fille de votre mère?");
+
+
+/*Category*/
+INSERT INTO category(name,is_active,description)
+VALUES("Peau douce",1,"Produits qui rends la peau douce." );
+
+INSERT INTO category(name,is_active,description)
+VALUES("Peau acide",1,"Produits pour la peau acide." );
+
+INSERT INTO category(name,is_active,description)
+VALUES("Rajeunissant",1,"Produits rajeunissant" );
+
+INSERT INTO category(name,is_active,description)
+VALUES("Peau sensible",1,"Produits pour la peau sensible." );
+
+INSERT INTO category(name,is_active,description)
+VALUES("Peau mature",1,"Produits pour peau mature." );
